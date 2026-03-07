a9d4d71c3ecf2e5af542103340828ae7513c7219
Yellapu Lakshmi Charan <73232493+ylcharan@users.noreply.github.com>
2026-03-07 12:12:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,6 +3,10 @@ class Solution {
         int[][] res = new int[k][2];
         PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> (x[0] * x[0] + x[1] * x[1]) - (y[0] * y[0] + y[1] * y[1]));
 
+        // (a, b) -> a - b (ascending)
+        // (a, b) -> b - a (descending)
+        // root of a < root of b === a < b
+
         for (int[] p: points) {
             pq.offer(p);
         }
