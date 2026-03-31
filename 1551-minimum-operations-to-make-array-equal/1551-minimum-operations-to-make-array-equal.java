c01d269eaa93e9afddd5deba553437745e8a6414
Yellapu Lakshmi Charan <73232493+ylcharan@users.noreply.github.com>
2026-03-31 21:45:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minOperations(int n) {
-        int[] arr = new int[n];
         int ops = 0;
         for (int i = 0; i< n / 2; i++) {
-            arr[i] = (i * 2) + 1; 
-            if (n % 2 == 0) ops += arr[i];
-            else ops += arr[i] + 1;
+            int temp = (i * 2) + 1; 
+            if (n % 2 == 0) ops += temp;
+            else ops += temp + 1;
         }
-        
         return ops;
     }
 }
